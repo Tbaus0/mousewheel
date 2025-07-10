4b260733017c4ee4a62783ef75cb5fa0a00a4b22
Tbaus0 <151596105+Tbaus0@users.noreply.github.com>
2025-07-10 17:55:52 -0400

    Update Wheel_serialrecorder.py
    output 2 graphs of position and velocity vs time

--- Wheel_serialrecorder.py
+++ Wheel_serialrecorder.py
@@ -10,11 +10,12 @@ import keyboard
 import time
 import serial
 import re     #regular expressions (regex)
+import matplotlib.pyplot as plt
+import numpy as np
 
 TIMEOUT = 0.1   #global timeout
 
-ser = serial.Serial('COM3', baudrate = 9600, timeout=TIMEOUT)
-wheelData = []
+ser = serial.Serial('COM3', baudrate = 9600, timeout=TIMEOUT) #begin serial monitor analysis
 
 #%% functions
 
@@ -23,17 +24,36 @@ def toggle_pause():
     paused = not paused
     print("Paused" if paused else "Resumed")
     
-def Get_numbers(string):
-    numbers = re.findall(r"[-+]?\d*\.\d+|\d+", string)
+def Get_numbers(data):
+    numbers = re.findall(r"[-+]?\d*\.\d+|\d+", data)
     
     return numbers
     
-#def Graph():
+def Graph_data(wheelData):
+    position = [row[0] for row in wheelData]
+    position = [float(p) for p in position]
+    velocity = [row[1] for row in wheelData]
+    velocity = [float(v) for v in velocity]
+    time = [row[2] for row in wheelData]
+    time = [float(t) for t in time]
+
     
-
-
+    x = np.arange(0,max(time),max(time)/len(time))
+    #plt.subplot2grid(2,1)
+    plt.figure(1)
+    plt.plot(x, position, label='position')
+    plt.title("Position vs time")
+    plt.legend()
+     
+    plt.figure(2)
+    plt.plot(x, velocity, label='position')
+    plt.title("Velocity vs time")
+    plt.legend()
+    
+    plt.show()
 
 #%% working code
+wheelData = []
 paused = True
 
 keyboard.on_press_key("space", lambda _: toggle_pause())    #when the space key is pressed, toggle pause variable boolean
@@ -45,8 +65,8 @@ while True:
         while keyboard.is_pressed("space"):
             if time.time() - start_time >= 0.5:  # spacebar held > 0.5 seconds
                 print("Spacebar held long — exiting program.")
-                ser.close()
-                #Graph(wheelData)
+                ser.close()                 
+                Graph_data(wheelData)
                 exit()
             time.sleep(TIMEOUT)
 
